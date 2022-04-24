feb925f98a33877e88d1cb307f4cf4cf387e4fe7
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-24 00:55:11 -0700

    Textbook progress

---
+++ _lanaguages.py
@@ -0,0 +1,9 @@
+favorite_lanaguages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+language = favorite_lanaguages['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
