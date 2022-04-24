13f450f2aad1665c683a2cda702b6f31cc17b931
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-24 01:06:22 -0700

    user dictionary completed

---
+++ user.py
@@ -0,0 +1,9 @@
+user_0 = {
+    'username': 'TheBuffSeagull',
+    'first': 'Trevor',
+    'last': 'Ball',
+}
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"value: {value}")
