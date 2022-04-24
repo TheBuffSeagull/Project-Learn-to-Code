16047a6fc7ec4335d84b797a86092d5c0f813043
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-24 11:28:28 -0700

    comments

--- user.py
+++ user.py
@@ -1,9 +1,9 @@
-user_0 = {
+user_0 = {  # making dictionary for my name
     'username': 'TheBuffSeagull',
     'first': 'Trevor',
     'last': 'Ball',
 }
 
-for key, value in user_0.items():
+for key, value in user_0.items(): # "key" and "value" can be any variable you want
     print(f"\nKey: {key}")
     print(f"value: {value}")
