a25bda16a2b339eb0207683ffac625a038d64174
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-24 12:35:38 -0700

    improvements on _languages

--- _lanaguages.py
+++ _lanaguages.py
@@ -5,5 +5,26 @@ favorite_lanaguages = {
     'phil': 'python',
 }
 
-language = favorite_lanaguages['sarah'].title()
-print(f"Sarah's favorite language is {language}.")
+for name, language in favorite_lanaguages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.\n")
+
+print("\n")
+
+# languages used so far
+for language in set(favorite_lanaguages.values()): # <---- set pulls all non duplicates from a dictionary 
+    print(language.title())
+
+print("\n")
+
+#names
+for name in favorite_lanaguages.keys(): # keys is not required as the output will be the same
+    print(name.title())
+
+print("\n")
+
+# values in dictionary
+for name in favorite_lanaguages.values(): # <---- values 
+    print(name.title())
+
+print("\n")
+
