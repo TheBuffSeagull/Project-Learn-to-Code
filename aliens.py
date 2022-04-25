0a06d2902e8d542717f3c262df5c08003346e950
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-24 19:18:25 -0700

    Working on Aliens

--- aliens.py
+++ aliens.py
@@ -1,19 +1,30 @@
-alien_0 = {
+aliens = []
+
+# make 30 green aliens
+for alien_number in range(31):
+    new_alien = { #dictionary to be replicated
     'color': 'green',
     'points': '5',
-}
+    'speed': '5',
+    }
 
-alien_1 = {
-    'color': 'yellow',
-    'points': '10',
-}
+    aliens.append(new_alien)
 
-alien_2 = {
-    'color': 'red',
-    'points': '15',
-}
+for alien in aliens[:3]:
+    if alien.get("color") == "green":
+        alien["color"] = "yellow"
+        alien["speed"] = "medium"
+        alien["points"] = "10"
+    elif alien.get("color") == "yellow":
+        alien["color"] = "red"
+        alien["speed"] = "fast"
+        alien["points"] = "15"
 
-aliens = [alien_0, alien_1, alien_2]
-
-for alien in aliens:
+#show the first 5 aliens.
+for alien in aliens[:5]:
     print(alien)
+
+print("...")
+
+#show how many aliens have been created.
+print(f"\nTotal number of aliens: {len(aliens)}")
