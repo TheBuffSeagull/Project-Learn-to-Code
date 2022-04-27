dbb73c0b53874ab5fc54afa7c875e475469bffe9
TheBuffSeagull <ball.trevor@outlook.com>
2022-04-27 16:55:53 -0700

    page 159

--- pizza_arguements.py
+++ pizza_arguements.py
@@ -3,7 +3,3 @@ def make_pizza(size, *toppings): # the "*"" next to toppings makes it into a tup
     print(f"\nMaking {size}-inch pizza with the folowing toppings:")
     for topping in toppings:
         print(f"- {topping}")
-
-
-make_pizza(16, 'pepperoni')
-make_pizza(12 ,'mushrooms', 'green peppers', 'extra cheese')
