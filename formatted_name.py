07ea96ca92a9d4e37b4cf50178b6a179d185c9b8
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-26 19:05:46 -0700

    page 141

--- formatted_name.py
+++ formatted_name.py
@@ -1,7 +1,21 @@
-def get_formatted_name(first_name, last_name):
+from setuptools import find_namespace_packages
+
+
+def get_formatted_name(first_name, last_name, middle_name = ""):
     """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
-musician = get_formatted_name('jimi', 'hendrix')
-print(musician)
+while True:
+    print("\nPlease tell me your name:")
+    print("\nEnter 'q' at any time to quit.")
+    f_name = input("First name:")
+    if f_name == 'q':
+        break
+    l_name = input("Last name:")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
