aa2ac8ea206b59d81a6bf2d84232acbca04eaa60
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-26 17:21:20 -0700

    update

--- Greeter.py
+++ Greeter.py
@@ -1,5 +1,6 @@
-prompt = "If you tell us who you are, we can personalize the messages you see."
-prompt += "\nWhat is your first name?: "
+def greet_user(username): # function definition tell python the name and info for the function inside '()'
+    """Display a simple  greeting.""" #doc strings are used in fuctions to describe what it does (needs 3 quotation marks)
+    name = username
+    print(f"\nHello, {name}")
 
-name = input(prompt)
-print(f"\nHello, {name}")
+greet_user('Trevor')
