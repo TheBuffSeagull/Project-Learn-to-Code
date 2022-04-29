48522ed2b90fdd369cffb2157faf61ccb4788162
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-28 17:28:58 -0700

    RENOVATIONS!

--- Python_Crash_Course/formatted_name.py
+++ Python_Crash_Course/formatted_name.py
@@ -1,4 +1,4 @@
-def get_formatted_name(first_name, last_name, middle_name = ""):
+def get_formatted_name(first_name, last_name, middle_name=None):
     """Return a full name, neatly formatted."""
     if middle_name:
         full_name = f"{first_name} {middle_name} {last_name}"
