348b95df341ec947fd4dad1a7bd256fe0c4e1eb0
TheBuffSeagull <ball.trevor@Outlook.com>
2022-04-29 12:04:51 -0700

    updates

---
+++ Python_Crash_Course/Dog_access.py
@@ -0,0 +1,4 @@
+import dog as Dog
+
+
+print(Dog("Willie", 6))
--- Python_Crash_Course/dog.py
+++ Python_Crash_Course/dog.py
@@ -1,8 +1,12 @@
+# A class is a set of instructions for how to make an instance
+# see line 21
 class Dog:
     """A simple attempt to model a dog."""
 
     def __init__(self, name, age):
         """Initialize name and age attributes."""
+        # The self paramater takes the paramater from the init above and turns them into attributes
+        # attributes are globally accessible in other functions like sit and roll_over below
         self.name = name
         self.age = age
 
@@ -13,3 +17,11 @@ class Dog:
     def roll_over(self):
         """simulate rolling over in response to a command."""
         print(f"{self.name} rolled over!")
+
+# We can use this one to make individual instances represeting specific dogs
+my_dog = Dog("Willie", 6)
+
+print(f"My Dog's name is {my_dog.name}.")
+print(f"My dog {my_dog.age} years old.")
+
+print(Dog.sit(my_dog))
