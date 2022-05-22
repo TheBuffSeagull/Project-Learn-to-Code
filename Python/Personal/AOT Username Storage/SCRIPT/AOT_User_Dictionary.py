000c95cb1feaafd76ab9437d98ba70bac1587432
TheBuffSeagull <ball.trevor@Outlook.com>
2022-05-22 07:10:50 -0700

    more changes -.-

--- Python/Personal/AOT Username Storage/SCRIPT/AOT_User_Dictionary.py
+++ Python/Personal/AOT Username Storage/SCRIPT/AOT_User_Dictionary.py
@@ -14,7 +14,7 @@ def cls():
 def clean_up(paste):
     """cleans off the bullshit from the init paste"""
     cleaning_front = paste.replace('<indent=2%>', '')
-    cleaning_back = cleaning_front.replace('</indent>', '')
+    cleaning_back = cleaning_front.replace('</indent>', '\n')
     final_cleaned_up_version = cleaning_back.replace('<color="orange">[YOU]</color>', '')
 
     return final_cleaned_up_version
@@ -41,7 +41,7 @@ def save_to_file(final):
 
     # reconstructing the data as a dictionary
 
-
+    print("Saving new file...")
     save_file = main_dictionary | final
 
     with open("USERNAMES.txt", "w") as f_w:
@@ -50,6 +50,34 @@ def save_to_file(final):
 
     print(f"File currently has {len(save_file)} values.\n\n\n")
 
+def read_paste_file():
+    with open('PASTE_DOC.txt', 'r') as f:
+        data = f.read()
+        print("Grabbing paste data from file as str: ", type(data))
+        return data
+
+
+def merge(list1, list2):
+      
+    merged_list = []
+    for i in range(max((len(list1), len(list2)))):
+  
+        while True:
+            try:
+                tup = (list1[i], list2[i])
+            except IndexError:
+                if len(list1) > len(list2):
+                    list2.append('')
+                    tup = (list1[i], list2[i])
+                elif len(list1) < len(list2):
+                    list1.append('')
+                    tup = (list1[i], list2[i])
+                continue
+  
+            merged_list.append(tup)
+            break
+    return merged_list
+
 
 origin_dict = {}
 used_list = []
@@ -57,36 +85,63 @@ used_list = []
 Active = True
 
 
+try:
+    edit = read_paste_file()
+    _edit = clean_up(edit)
+
+    user_name = re.findall('"([^"]*)"', _edit) 
+    user_ID = re.findall(r'\s\[(\w+)\]', _edit)
+
+    print(f"Found {user_name} with ID {user_ID}\n")
+                    
+    import_data = merge(user_ID, user_name)
+
+    for _id, _name in import_data:
+        if _id not in origin_dict:
+            origin_dict[_id] = list()       
+        if _name not in used_list:
+            origin_dict[_id].append(_name)
+            used_list.append(_name)
+
+    pprint.pprint(origin_dict)
+except Exception as e: print(e)
+
+
 while Active: 
-    string = input("add, search, or exit: ")
+    string = input("Type an ID, 'all', or 'exit': ")
 
     if string == "add":
         edit_mode = True
         while edit_mode:
-            edit = clean_up(input("Add data or save: "))
+            edit = input("'import' or 'save': ")
+            if edit == "import":
+                try:
+                    edit = read_paste_file()
+                    _edit = clean_up(edit)
+
+                    user_name = re.findall('"([^"]*)"', _edit) 
+                    user_ID = re.findall(r'\s\[(\w+)\]', _edit)
+
+                    print(f"Found {user_name} with ID {user_ID}\n")
+                    
+                    import_data = merge(user_ID, user_name)
+
+                    for _id, _name in import_data:
+                        if _id not in origin_dict:
+                            origin_dict[_id] = list()       
+                        if _name not in used_list:
+                            origin_dict[_id].append(_name)
+                            used_list.append(_name)
+
+                    pprint.pprint(origin_dict)
+                except Exception as e: print(e)
+            
             #creating an exit
             if edit == "save":
+                cls()
                 save_to_file(origin_dict)
                 edit_mode = False
                 break
-            
-            user_name = re.findall('"([^"]*)"', edit) 
-            user_ID = re.findall(r'\s\[(\w+)\]', edit)
-
-            print(f"Found {user_name} with ID {user_ID}\n")
-
-            _name = "".join(user_name)
-            _id = "".join(user_ID)
-
-
-            if _id not in origin_dict:
-                origin_dict[_id] = list()
-            if _name not in used_list:
-                origin_dict[_id].append(_name)
-
-
-            seen = set(used_list)
-            used_list.append(_name)
 
 
     if string == "search":
