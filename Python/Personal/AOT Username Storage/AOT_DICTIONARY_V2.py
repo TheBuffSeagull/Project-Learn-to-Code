31b3653ee0426bcf7c863ef131ad0194442288b9
TheBuffSeagull <ball.trevor@Outlook.com>
2022-06-01 17:48:04 -0700

    done

--- Python/Personal/AOT Username Storage/AOT_DICTIONARY_V2.py
+++ Python/Personal/AOT Username Storage/AOT_DICTIONARY_V2.py
@@ -3,6 +3,69 @@ from pathlib import Path
 import pprint
 import re
 import json
+import os
+
+def add_data(data, Dictionary):
+    """Adds incoming data to Dictionary. It requires the data be a list with _ID 1st and _name 2nd"""
+    
+    for _id, _name in data:
+        if _id not in Dictionary.keys():
+            Dictionary[_id] = list()      
+        if _name not in Dictionary.get(_id):
+            Dictionary[_id].append(_name)
+            #Telling the user about new entries cause its fun
+            print(f"Adding {_name} to ID: {_id}")
+    
+    #move back to first line of file
+    file.seek(0)
+    #Write dictionary to Name_STORAGE.txt 
+    # (dumps makes dictionary to str cause you can't write DICT to file)
+    file.write(json.dumps(Dictionary))
+    #truncate makes sure the file is now the size of "Dictionary" (sometimes old stuff was left behind)
+    file.truncate()
+    #get current number of entries
+
+
+
+def get_chatlog(Dictionary):
+    """Gets the chatlog history if user has enabled logging"""
+    #placeholders
+
+    data = []
+    #getts user name
+    Windows_user = os.getlogin()
+    #finds chatlogs with windows username
+    directory = rf"C:\Users\{Windows_user}\AppData\LocalLow\RoarkInc\raot\chatlogs"
+
+    with open(f"{directory}\chatlog.txt", 'r', encoding="utf8") as chatlog:
+        
+        #reading chatlog and saving data as chatlog >_> I'm lazy with names
+        chatlog = chatlog.readlines()
+        
+        for lns in chatlog:
+            if lns.startswith('User'):
+                data.append(lns)
+
+        for ln in data:
+            #Clean up lines
+            ln = ln.replace('Username: ', '')
+            ln = ln.replace(' Id: ', '')
+            ln = ln.replace('\n', '')
+            ln = ln.replace(' ', '')
+            items = ln.split('|')
+            items.reverse()
+
+            _id = items[0::2]
+            _name = items[1::2]
+
+
+
+            items = merge(_id, _name)
+
+            add_data(items, Dictionary)
+    
+
+
 
 
 def get_paste(data):  
@@ -16,7 +79,6 @@ def get_paste(data):
     #SPECIFYING THESE BEFORE CLEANING TO AVOID BAD BRACKETS BEING ADDED LIKE HOST DATA NUMBERS Ex: '[01] -'
     user_name = re.findall('"([^"]*)"', data) 
     user_ID = re.findall(r'\s\[(\w+)\]', data)
-    
 
 
     data = merge(user_ID, user_name)
@@ -51,6 +113,7 @@ def merge(list1, list2):
 #making sure the file exists with touch before opening it
 Path('NAME_STORAGE.txt').touch(exist_ok=True)
 
+print("If you are hosting please enable logging in console to auto update in host mode!~")
 
 with open("NAME_STORAGE.txt", "r+") as file:
     data = file.read()
@@ -70,48 +133,23 @@ with open("NAME_STORAGE.txt", "r+") as file:
     #main user experience
     Active = True
     while Active:
-        
-        user_input = input("Type an AOT User ID, 'add', 'all', 'remove', or 'exit': ")
-
-   
+    
+        user_input = input("Type an AOT User ID, 'add', 'all', 'remove', 'host', or 'exit': ")
+                
+    
         if user_input == 'add':
             while True:
                 paste_input = input("Please paste new data into window or type 'save': ")
                 
                 #check for user exit before process.
-                if paste_input == 'save':
-                    #save current stage in case someone closes the program without typing exit
-                    try:
-                        #merging current updated dictionary and paste entries dictionary
-                        #already added to dictionary. Might not be needed
-                        Dictionary = Dictionary | paste_doc
-                    except Exception as e: print(e)
-
-
-                    #move back to first line of file
-                    file.seek(0)
-                    #Write dictionary to Name_STORAGE.txt 
-                    # (dumps makes dictionary to str cause you can't write DICT to file)
-                    file.write(json.dumps(Dictionary))
-                    #truncate makes sure the file is now the size of "Dictionary" (sometimes old stuff was left behind)
-                    file.truncate()
-                    #get current number of entries
-                    print(f"File has updated to have {len(Dictionary)} values.\n")
-                    
+                if paste_input == 'save':                
                     break
                 
                 #look for new entries from user paste
                 paste_doc = get_paste(paste_input)
+                print(paste_doc)
 
-                paste_doc = set(paste_doc)
-
-                for _id, _name in paste_doc:
-                    if _id not in Dictionary.keys():
-                        Dictionary[_id] = list()
-                        print(f"Adding Dictionary ID: {_id}")       
-                    if _name not in Dictionary.get(_id):
-                        Dictionary[_id].append(_name)
-                        print(f"Adding Name: {_name}")
+                add_data(paste_doc, Dictionary)
                 
 
 
@@ -137,7 +175,16 @@ with open("NAME_STORAGE.txt", "r+") as file:
         
         if user_input == 'all':
             pprint.pprint(Dictionary)
-            print(f"File currently has {len(Dictionary)} values.\n")
+            print(f"File now has {len(Dictionary)} values.\n")
+
+        if user_input == 'host':
+            print("Host mode enabled. Press ctrl-c to stop: ")
+            try:
+                while True:
+                    get_chatlog(Dictionary)
+            except KeyboardInterrupt:
+                pass
+
 
         #get user ID if someone looks it up
         elif user_input in Dictionary:
@@ -154,15 +201,6 @@ with open("NAME_STORAGE.txt", "r+") as file:
         if user_input == 'exit':
             Active = False
     
-    
-    
-    #Start closing the program
-    try:
-        #merging current updated dictionary and paste entries dictionary
-        Dictionary = Dictionary | paste_doc
-    except Exception as e: print(e)
-
-
     #move back to first line of file
     file.seek(0)
     #Write dictionary to Name_STORAGE.txt 
